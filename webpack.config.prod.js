4daf105a4feb27e3f383bb1f51410f75034b8239
sterloin <silversterling@gmail.com>
2017-01-07 12:56:39 -0500

    update readme and prod

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,8 @@
 var path = require('path');
 var webpack = require('webpack');
-
+var rucksack = require('rucksack-css');
 module.exports = {
+  // or devtool: 'eval' to debug issues with compiled output:
   devtool: 'source-map',
   entry: [
     './src/index'
@@ -9,9 +10,14 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/dist/'
   },
   plugins: [
+    new webpack.ProvidePlugin({   
+        jQuery: 'jquery',
+        $: 'jquery',
+        jquery: 'jquery'
+    }),
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
@@ -25,10 +31,56 @@ module.exports = {
     })
   ],
   module: {
-    loaders: [{
+    loaders: [
+    {
       test: /\.js$/,
       loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
+      includes: [path.join(__dirname, 'src'), path.join(__dirname, 'imp_pat_modules')],
+      exclude: /node_modules/
+    },
+    {
+      test: /\.css$/,
+      loaders: ['style', 'css'],
+      includes: [
+        path.join(__dirname, 'src'),
+        path.join(__dirname, 'node_modules/dist/css'),
+        path.join(__dirname, 'imp_pat_modules')
+      ],
+    },
+    {
+      test: /\.pcss$/,
+      loaders: ['style', 'css-loader?modules&sourceMap&importLoaders=1&localIdentName=[local]___[hash:base64:5]', 'postcss'],
+      includes: [
+        path.join(__dirname, 'src'),
+        path.join(__dirname, 'node_modules/dist/css'),
+        path.join(__dirname, 'imp_pat_modules')
+      ],
+    },
+    { test: /\.json$/, loader: 'json' },
+    { test: /\.scss$/, loaders: ['style', 'css', 'postcss', 'sass'] },
+    {
+      test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=application/font-woff"
+    }, {
+      test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=application/font-woff"
+    }, {
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=application/octet-stream"
+    }, {
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "file"
+    }, {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&mimetype=image/svg+xml"
+    },
+    { test: /bootstrap\/dist\/js\/umd\//, loader: 'imports?jQuery=jquery' }
+    ]
+  },
+  postcss: [
+    rucksack({
+      autoprefixer: true
+    }),
+  ],
 };
+
